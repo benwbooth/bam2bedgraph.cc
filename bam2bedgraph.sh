68a42e4f84b5e4b708ec351ec78c59f701958265
Ben Booth <benwbooth@gmail.com>
2015-08-11 10:46:26 -0700

    add DEBUG flag to wrapper script

--- bam2bedgraph.sh
+++ bam2bedgraph.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 
 make SHELL='bash -eu' -s -f - "$BIN" -j <<'EOF'
 -include $(BIN).d
-CXXFLAGS= -O2 -march=native -pipe -std=c++1y -Wall -Wextra -pedantic -I/usr/local/include/bamtools -I/usr/include/bamtools
+CXXFLAGS= $(if $(DEBUG),-g -O0,-O2) -march=native -pipe -std=c++1y -Wall -Wextra -pedantic -I/usr/local/include/bamtools -I/usr/include/bamtools
 LDLIBS= -lbamtools -lboost_program_options -lboost_filesystem -lboost_system -lboost_regex
 $(BIN) : $(SRC) $(SCRIPT)
 	mkdir -m 700 -p "$(BINDIR)" "$$(dirname "$@")" && \
