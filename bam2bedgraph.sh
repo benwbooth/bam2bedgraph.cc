6f3639ec36936a9105c8e7be56cc008a6a62b69f
Ben Booth <benwbooth@gmail.com>
2015-08-11 10:56:16 -0700

    make wrapper script work for mac os

--- bam2bedgraph.sh
+++ bam2bedgraph.sh
@@ -11,12 +11,13 @@ make SHELL='bash -eu' -s -f - "$BIN" -j <<'EOF'
 CXXFLAGS= $(if $(DEBUG),-g -O0,-O2) -march=native -pipe -std=c++1y -Wall -Wextra -pedantic -I/usr/local/include/bamtools -I/usr/include/bamtools
 LDLIBS= -lbamtools -lboost_program_options -lboost_filesystem -lboost_system -lboost_regex
 $(BIN) : $(SRC) $(SCRIPT)
-	mkdir -m 700 -p "$(BINDIR)" "$$(dirname "$@")" && \
+	mkdir -p "$$(dirname "$@")" && \
 	TMP=$$(TMPDIR=$$(dirname "$@") mktemp -t "tmpXXXXXX") && \
 	$(LINK.cc) -MMD -MP -MF "$$TMP.d" -MQ "$@" "$<" $(LDLIBS) -o "$$TMP" && \
 	perl -i -pe "s/<stdin>//" "$$TMP.d" && \
 	mv "$$TMP.d" "$@.d" && \
-	mv "$$TMP" "$@"
+	mv "$$TMP" "$@" && \
+	chmod +x "$@"
 EOF
 
 exec -a "$0" "$BIN" "$@"
